84ca5ddf66e45e280c7c948fd0471265d6c34ebd
Andrey Bychkov <mrdrew@altlinux.org>
2020-07-30 12:03:47 +0300

    Add size detector gui class with needed functions

--- sizedetectorgui.py
+++ sizedetectorgui.py
@@ -3,8 +3,15 @@ from subprocess import TimeoutExpired
 
 from sizedetector_core import SizeDetectorCore
 
+import sys
 import os
 import psutil
+
+from PySide2.QtWidgets import (QApplication, QLabel, QPushButton,
+                               QVBoxLayout, QWidget, QLineEdit, QTableWidget,
+                               QTableWidgetItem, QHeaderView)
+from PySide2.QtCore import Qt
+from PySide2 import QtGui
 
 import logging
 
@@ -77,3 +84,140 @@ class SizeDetector:
             return False, message
 
         return True, out.decode().split('\n')
+
+
+class SizeDetectorGUI(QWidget):
+    EXIT_CODE_REBOOT = -123
+    PREVIOUS_GEOMETRY = None
+
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.app_initialize = False
+
+        self.struct = SizeDetectorCore.get_config_params()
+
+        self.gs_button = QPushButton(text="Detect Size!")
+        self.ref_button = QPushButton(text="Refresh")
+
+        self.inf_label = QLabel("Enter directory for sizing:")
+        self.inf_label.setAlignment(Qt.AlignLeft)
+
+        self.inp_line = QLineEdit("/")
+
+        self.res_table = QTableWidget()
+        self.res_table.setColumnCount(2)
+        self.res_table.setHorizontalHeaderItem(0, QTableWidgetItem("size"))
+        self.res_table.setHorizontalHeaderItem(1, QTableWidgetItem("file"))
+
+        header = self.res_table.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.Stretch)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.inf_label)
+        self.layout.addWidget(self.inp_line)
+        self.layout.addWidget(self.gs_button)
+        self.layout.addWidget(self.res_table)
+        self.layout.addWidget(self.ref_button)
+
+        self.setLayout(self.layout)
+
+        self.gs_button.clicked.connect(self.run_detecting)
+        self.ref_button.clicked.connect(self.__refresh)
+
+        self.__initialize()
+
+    def __initialize(self):
+        if self.struct is None:
+            self._disable_form()
+            self._set_info_message("Invalid or missing config file.\n"
+                                   "For detailed information see log file")
+            return
+
+        if self.struct['pmanager'] != 'rpm':
+            self._disable_form()
+            self._set_info_message("Unsupported package manager {}"
+                                   "".format(self.struct['pmanager']))
+            return
+
+        shell = 'rpm -qa {}'.format(self.struct['dupackage'])
+        try:
+            proc = Popen(shell.split(), stdout=PIPE, stderr=PIPE)
+        except FileNotFoundError as e:
+            logger.debug("Command `{}` failed: {}".format(shell, e.strerror))
+            self._set_info_message(
+                "Runtime error. For detailed information see log file.")
+            return
+
+        out, err = proc.communicate()
+
+        if err:
+            logger.debug("Command `{}` failed: {}".format(shell, err.decode()))
+            self._set_info_message(
+                "Runtime error. For detailed information see log file.")
+            return
+
+        if not out:
+            self._disable_form()
+            self._set_info_message("No package `{}` found on the system"
+                                   .format(self.struct['dupackage']))
+            return
+
+        self.app_initialize = True
+
+    def run_detecting(self):
+        self.inf_label.setText("Enter directory for sizing:")
+        dir_ = self.inp_line.text()
+        if dir_[-1] != '/':
+            dir_ += '/'
+
+        sd = SizeDetector(dir_)
+        status, retval = sd.detect_size()
+        if status is False:
+            self._set_info_message(retval)
+            return
+
+        self.res_table.setRowCount(len(retval) - 1)
+
+        count = 0
+        for line in retval:
+            if line:
+                line = line.split()
+                self.res_table.setItem(count, 0, QTableWidgetItem(line[0]))
+                dir_name = line[1].replace(dir_, '')
+                if len(retval) - 2 == count:
+                    dir_name = '* Summary size *'
+                self.res_table.setItem(count, 1, QTableWidgetItem(dir_name))
+                count += 1
+
+    def _set_info_message(self, message):
+        self.inf_label.setText(message)
+
+    def _disable_form(self):
+        self.inp_line.setDisabled(True)
+        self.gs_button.setDisabled(True)
+        self.ref_button.setFocus()
+
+    def __refresh(self):
+        SizeDetectorGUI.PREVIOUS_GEOMETRY = self.geometry()
+        QtGui.qApp.exit(SizeDetectorGUI.EXIT_CODE_REBOOT)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setApplicationDisplayName(APPLICATION_NAME)
+
+    current_exit_code = SizeDetectorGUI.EXIT_CODE_REBOOT
+    while current_exit_code == SizeDetectorGUI.EXIT_CODE_REBOOT:
+        widget = SizeDetectorGUI()
+        if widget.app_initialize:
+            win_size = widget.struct['winsize'].split(',')
+            WIDTH = int(win_size[0].replace(' ', ''))
+            HEIGHT = int(win_size[1].replace(' ', ''))
+        widget.resize(WIDTH, HEIGHT)
+        if widget.PREVIOUS_GEOMETRY:
+            widget.setGeometry(widget.PREVIOUS_GEOMETRY)
+
+        widget.show()
+        current_exit_code = app.exec_()
